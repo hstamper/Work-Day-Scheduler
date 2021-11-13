216e5c9bb45902888f41f0352fb7a8a24ff2f4e4
Hilliard Stamper <hilliard.stamper@att.net>
2021-11-13 15:13:56 -0500

    added varibles

---
+++ script.js
@@ -0,0 +1,10 @@
+
+$("#9am").attr("date-time", moment("9:00am", "h:mm a").format("HH")); 
+$("#10am").attr("date-time", moment("10:00am", "h:mm a").format("HH"));
+$("#11am").attr("date-time", moment("11:00am", "h:mm a").format("HH")); 
+$("#12pm").attr("date-time", moment("12:00pm", "h:mm a").format("HH"));
+$("#1pm").attr("date-time", moment("1:00pm", "h:mm a").format("HH"));
+$("#2pm").attr("date-time", moment("2:00pm", "h:mm a").format("HH"));
+$("#3pm").attr("date-time", moment("3:00pm", "h:mm a").format("HH"));
+$("#4pm").attr("date-time", moment("4:00pm", "h:mm a").format("HH"));
+$("#5pm").attr("date-time", moment("5:00pm", "h:mm a").format("HH"));
