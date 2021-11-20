5ef77ea6bac8170a12c40310e23c873d22e1623f
Hilliard Stamper <hilliard.stamper@att.net>
2021-11-20 13:10:55 -0500

    updated function to save data

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
-$("#9am").attr("date-time", moment("9:00am", "h:mm a").format("HH")); 
+$("#9am").attr("date-time", moment("9:00am", "h:mm a").format("HH"));
 $("#10am").attr("date-time", moment("10:00am", "h:mm a").format("HH"));
-$("#11am").attr("date-time", moment("11:00am", "h:mm a").format("HH")); 
+$("#11am").attr("date-time", moment("11:00am", "h:mm a").format("HH"));
 $("#12pm").attr("date-time", moment("12:00pm", "h:mm a").format("HH"));
 $("#1pm").attr("date-time", moment("1:00pm", "h:mm a").format("HH"));
 $("#2pm").attr("date-time", moment("2:00pm", "h:mm a").format("HH"));
@@ -14,30 +14,53 @@ var time = moment();
 function setPlanner() {
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
-    $(".time-block").each(function() {
-        var id = $(this).attr("id"); 
-        var schedule = JSON.parse(localStorage.getItem("schedule"));
+    $(".time-block").each(function () {
+        var id = $(this).attr("id");
+       
+         var schedule = JSON.parse(localStorage.getItem(id));
+         console.log(id, schedule);
 
-        if (schedule !== null) {
-            $(this).children(".description").val(schedule); 
-        }
+         $(this).children(".description").val(schedule);
+
+        // if (schedule !== null) {
+        //     $(this).children(".description").val(schedule); 
+        // }
     });
 }
 
-setPlanner(); 
+setPlanner();
 
-var saveBtn = $(".savebtn"); 
 
-saveBtn.on("click", function() {
-    var time = $(this).parent().attr("id"); 
-    var schedule = $(this).siblings(".description").val().trim(); 
+$(".saveBtn").click(function () {
+    var time = $(this).parent().attr("id");
+    var schedule = $(this).siblings(".description").val().trim();
 
-    localStorage.setItem("time", JSON.stringify(time));
-    localStorage.setItem("schedule", JSON.stringify(schedule)); 
-
-    localStorage.getItem("time"); 
-    localStorage.getItem("schedule"); 
-
+    localStorage.setItem(time, schedule);
+    
+    console.log("test");
 });
+
+
+function pastPresentFuture() {
+    hour = time.hours();
+    $(".time-block").each(function () {
+        var thisHour = parseInt($(this).attr("date-time"));
+
+        if (thisHour > hour) {
+            $(this).addClass("future")
+        }
+        else if (thisHour === hour) {
+            $(this).addClass("present");
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+        }
+        else {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+        }
+    })
+}
+
+pastPresentFuture();
 
 
